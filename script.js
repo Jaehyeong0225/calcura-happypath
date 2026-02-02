7d9ccbafed72e2926da2b7f4dd59e1064e99eb25
Jaehyeong0225 <jhgaliys11@gmail.com>
2026-02-02 16:00:34 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("loginBtn").onclick = function () {
+    window.location.href = "home.html";
+};
+
+document.getElementById("guestBtn").onclick = function () {
+    window.location.href = "home.html";
+};
+
+document.getElementById("signupBtn").onclick = function () {
+    window.location.href = "signup.html";
+};
