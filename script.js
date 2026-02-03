86414b2e2e5a9cc0e54df6eb0eb46d38e522d0bc
Jaehyeong0225 <jhgaliys11@gmail.com>
2026-02-03 16:59:12 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-document.getElementById("loginBtn").onclick = function () {
-    window.location.href = "home.html";
-};
+const emailEl = document.getElementById("email");
+const pwEl = document.getElementById("password");
 
-document.getElementById("guestBtn").onclick = function () {
-    window.location.href = "home.html";
-};
+document.getElementById("loginBtn").addEventListener("click", () => {
+  const email = emailEl.value.trim();
+  const pw = pwEl.value.trim();
 
-document.getElementById("signupBtn").onclick = function () {
-    window.location.href = "signup.html";
-};
+  window.location.href = "home.html";
+});
+
+document.getElementById("guestBtn").addEventListener("click", () => {
+  window.location.href = "home.html?guest=true";
+});
+
+document.getElementById("signupBtn").addEventListener("click", () => {
+  window.location.href = "signup.html";
+});
+
+document.getElementById("forgotBtn").addEventListener("click", () => {
+  window.location.href = "forgot.html";
+});
